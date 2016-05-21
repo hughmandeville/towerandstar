e62c4570ea3c8eec46774d80d83f1918181d0b70
Hugh Mandeville <hubert.mandeville@nytimes.com>
2016-05-21 16:05:54 -0400

    Set band name using css.

--- code.js
+++ code.js
@@ -7,50 +7,9 @@ $(function() {
     if ( ! window.console ) {
         console = { log: function(){} };
     }
-    update_band_name();
-    // Update band name font size and position, max every 100ms
-    setInterval(update_band_name, 100);
-    $(window).resize(function() {
-        needs_resizing = true;
-    });
 });
 
 
-function update_band_name() {
-    if (needs_resizing == false) {
-        return;
-    }
-    needs_resizing = false;
-
-    $("#band_name").css("width", "inherit");
-    var band_name_width = $("#band_name").width() + 80;
-    var photo_width = $("#group_photo").outerWidth(true);
-
-    var font_size = parseInt($("#band_name").css("font-size"));
-
-    
-    if (band_name_width > photo_width) {
-        while (band_name_width > photo_width) {
-            font_size--;
-            $("#band_name").css("font-size", font_size);
-            band_name_width = $("#band_name").width() + 80;
-        }       
-    } else if (band_name_width < photo_width) {
-        while (band_name_width < photo_width) {
-            font_size++;
-            $("#band_name").css("font-size", font_size);
-            band_name_width = $("#band_name").width() + 80;
-        }
-    }
-    
-    $("#band_name").css("width", "100%");
-    
-    var top = $("#group_photo").position().top + $("#group_photo").outerHeight(true) - $("#band_name").outerHeight(true);
-    
-    $("#band_name").css("top", top);
-    
-    //$("#main_image").css("height", $(window).height());
-}
 
 
 
