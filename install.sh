94cb755bd4580758f9589fe3957e520b258a7a80
Hugh Mandeville <hubert.mandeville@nytimes.com>
2016-06-30 10:07:36 -0400

    install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+scp code.js tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html
+scp index.html tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html
+scp index2.html tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html
+scp style.css tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html
+#scp images/*jpg tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html/images/
+#scp images/*png tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html/images/
+
+
+
