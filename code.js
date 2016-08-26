364c40783e34fef9a7db434e4640752d319f0f00
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2016-08-26 08:36:55 -0400

    Removed old code

--- code.js
+++ code.js
@@ -5,15 +5,3 @@ $(function() {
     }
 
 });
-
-
-var image_id = 1;
-function update_images() {
-    var post = "c_";
-
-    console.log("update images");
-    for (i=1; i <= 6; i++) {
-        $(".box" + i).css("background-image", "url('images/tanya_rose_c_" + (((image_id + i) % 6) + 1) + ".jpg')");
-    }
-    image_id++;
-}
