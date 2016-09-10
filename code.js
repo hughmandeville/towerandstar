b4d32837c5b3c57bd8aeb247e3a3a10ebf7a48f6
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2016-09-10 12:22:28 -0400

    Added bio expand collapse

--- code.js
+++ code.js
@@ -4,4 +4,16 @@ $(function() {
         console = { log: function(){} };
     }
 
+    $(".bio_expand_collapse").on("click", function() {
+        var expand_collapse = $(this).text();
+        if (expand_collapse == "expand_more") {
+            $(this).text("expand_less");
+            $(this).parent().parent().find(".bio").show();
+            $(this).attr("title", "collapse");
+        } else {
+            $(this).text("expand_more");
+            $(this).parent().parent().find(".bio").hide();
+            $(this).attr("title", "expand");
+        }
+    });
 });
