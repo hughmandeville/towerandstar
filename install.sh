b35d2174e51c250530b14ee9a0b399cc1fb89a5e
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2016-09-10 12:23:54 -0400

    added images line.

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 scp code.js tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html
 scp index.html tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html
-scp index2.html tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html
+#scp index2.html tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html
 scp style.css tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html
 #scp images/*jpg tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html/images/
 #scp images/*png tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html/images/
+
+#scp images/media_*.jpg tanyadonovan@towerandstar.com:/home/tanyadonovan/public_html/images/
 
 
 
