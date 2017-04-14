6006dcda551481a935ab6c9031da0d2e1b578634
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2017-04-13 21:37:22 -0400

    updating heading clicks.

--- code.js
+++ code.js
@@ -1,19 +1,17 @@
-$(function() {    
+$(function() {
     // If browser doesn't support console log (like IE) set to empty function, so don't get errors.
     if ( ! window.console ) {
         console = { log: function(){} };
     }
-
-    $(".bio_expand_collapse").on("click", function() {
-        var expand_collapse = $(this).text();
-        if (expand_collapse == "expand_more") {
-            $(this).text("expand_less");
-            $(this).parent().parent().find(".bio").show();
-            $(this).attr("title", "collapse");
-        } else {
-            $(this).text("expand_more");
-            $(this).parent().parent().find(".bio").hide();
-            $(this).attr("title", "expand");
-        }
+    $(".heading").on("click", function() {
+        $(this).parent().find(".details").toggle();
+        var img = $(this).parent().data("img");
+        $("html").css("background-image", "url('images/" + img + "')");
+    });
+    $(".heading").on("mouseover", function() {
+        var img = $(this).parent().data("img");
+        $("html").css("background-image", "url('images/" + img + "')");
     });
 });
+
+
