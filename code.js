ba1ab3730f6a1d11e30498874bc22d194d12da7d
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2021-03-23 21:31:53 -0400

    added yt ids

--- code.js
+++ code.js
@@ -1,44 +1,48 @@
 var needs_resizing = true;
 var selected = null;
 
-$(function() {
-    if ( ! window.console ) {
-        console = { log: function(){} };
-    }
-    var rand = Math.floor((Math.random() * 2));
-    if (rand == 0) {
-        $("#video_iframe").attr("src", "http://www.youtube.com/embed/pKX3wbSKxF8?controls=1&fs=1&modestbranding=1");
-    }
-    update_video_size();
-    // Update image size at max every 100ms
-    setInterval(update_video_size, 100);
-    $(window).resize(function() {
-        needs_resizing = true;
-    });
+$(function () {
+  if (!window.console) {
+    console = { log: function () {} };
+  }
+  // YouTube IDs
+  const ytIDs = ["0pSCxHU3t54", "E_bcANrhy9c", "pKX3wbSKxF8", "Y3MSQkm3PXY"];
+  var rand = Math.floor(Math.random() * ytIDs.length);
+  $("#video_iframe").attr(
+    "src",
+    "http://www.youtube.com/embed/" +
+      ytIDs[rand] +
+      "?controls=1&fs=1&modestbranding=1"
+  );
 
-    $(".item").on("click", function() {
-        if (selected != null) {
-            $("#links_" + selected).hide();
-        }
-        var new_selected = $(this).text();
-        if (new_selected == selected) {
-            selected = null;
-        } else {
-            selected = new_selected;
-            if (selected != null) {
-                $("#links_" + selected).show();
-            }
-        }
-    });
+  update_video_size();
+  // Update image size at max every 100ms
+  setInterval(update_video_size, 100);
+  $(window).resize(function () {
+    needs_resizing = true;
+  });
+
+  $(".item").on("click", function () {
+    if (selected != null) {
+      $("#links_" + selected).hide();
+    }
+    var new_selected = $(this).text();
+    if (new_selected == selected) {
+      selected = null;
+    } else {
+      selected = new_selected;
+      if (selected != null) {
+        $("#links_" + selected).show();
+      }
+    }
+  });
 });
 
-
 function update_video_size() {
-    if (needs_resizing == false) {
-        return;
-    }
-    needs_resizing = false;
-    $("#video_div").css("height", ($(window).height() - 50));
-    console.log("update video size: " + ($(window).height() - 50));
+  if (needs_resizing == false) {
+    return;
+  }
+  needs_resizing = false;
+  $("#video_div").css("height", $(window).height() - 50);
+  //console.log("update video size: " + ($(window).height() - 50));
 }
-
